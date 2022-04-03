44d8c71e3555ae7653942f419db6500e788ce644
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-04 01:02:55 +0600

    header style add

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>This is header</h1>
+  return (
+    <div>
+      <div className="sticky top-0 bg-blue-200 px-12 py-8 flex justify-center">
+        <div className="flex gap-4 text-xl">
+          <Link className="border-b-2 border-blue-800 text-blue-900" to="/home">
+            Home
+          </Link>
+          <Link className="border-b-2 border-blue-800 text-blue-900" to="/reviews">
+            Reviews
+          </Link>
+          <Link className="border-b-2 border-blue-800 text-blue-900" to="/dashboard">
+            Dashboard
+          </Link>
+          <Link className="border-b-2 border-blue-800 text-blue-900" to="/blogs">
+            Blogs
+          </Link>
+          <Link className="border-b-2 border-blue-800 text-blue-900" to="/about">
+            About
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;

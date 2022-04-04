a0a58ef2ab61f536cf498c3814ba150558553279
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-04 19:54:22 +0600

    about page add

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,83 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
+  return (
+    <section class="px-4 pt-10 pb-24 mx-auto max-w-7xl md:px-2">
+      <h1 class="mb-10 text-2xl font-bold text-center text-[#0a0558] md:text-3xl">
+        How to write a book review
+      </h1>
+      <div class="grid grid-cols-1 gap-24 md:grid-cols-2">
         <div>
-            <h1>This is about page</h1>
+          <p class="text-gray-600">
+            Author Luisa Plaja offers her top tips for how to write a brilliant
+            review of the latest book you read - whether you liked it or not.
+            Other readers will always be interested in your opinion of the books
+            you've read. Whether you've loved the book or not, if you give your
+            honest and detailed thoughts then people will find new books that
+            are right for them. If you're stuck on what to say in a review, it
+            can help to imagine you're talking to someone who's asking you
+            whether they should read the book.
+          </p>
+          <p class="mt-10 mb-3 font-semibold text-gray-900">
+            1. Start with a couple of sentences describing what the book is
+            about
+          </p>
+          <p class="text-gray-600">
+            But without giving any spoilers or revealing plot twists! As a
+            general rule, try to avoid writing in detail about anything that
+            happens from about the middle of the book onwards. If the book is
+            part of a series, it can be useful to mention this, and whether you
+            think you'd need to have read other books in the series to enjoy
+            this one.
+          </p>
+          <p class="mt-10 mb-3 font-semibold text-gray-900">
+            2. Discuss what you particularly liked about the book
+          </p>
+          <p class="text-gray-600">
+            Focus on your thoughts and feelings about the story and the way it
+            was told. You could try answering a couple of the following
+            questions: Who was your favourite character, and why? Did the
+            characters feel real to you? Did the story keep you guessing? What
+            was your favourite part of the book, and why? Were certain types of
+            scene written particularly well - for example sad scenes, tense
+            scenes, mysterious ones...? Did the book make you laugh or cry? Did
+            the story grip you and keep you turning the pages?
+          </p>
         </div>
-    );
+        <div>
+          <p class="mt-10 mb-3 font-semibold text-gray-900">
+            3. Mention anything you disliked about the book
+          </p>
+          <p class="text-gray-600">
+            Talk about why you think it didn't work for you. For example: Did
+            you wish the ending hadn't been a cliffhanger because you found it
+            frustrating? Did you find it difficult to care about a main
+            character, and could you work out why? Was the story too scary for
+            your liking, or did it focus on a theme you didn't find interesting?
+          </p>
+          <p class="mt-10 mb-3 font-semibold text-gray-900">
+            4. Round up your review
+          </p>
+          <p class="text-gray-600">
+            Summarise some of your thoughts on the book by suggesting the type
+            of reader you'd recommend the book to. For example: younger readers,
+            older readers, fans of relationship drama/mystery stories/comedy.
+            Are there any books or series you would compare it to?
+          </p>
+          <p class="mt-10 mb-3 font-semibold text-gray-900">
+            5. You can give the book a rating, for example a mark out of five or
+            ten, if you like!
+          </p>
+          <p class="text-gray-600">
+            Luisa Plaja loves words and books, and she used to edit the book
+            review site Chicklish. Her novels for teenagers include Split by a
+            Kiss, Swapped by a Kiss and Kiss Date Love Hate. She lives in Devon,
+            England, and has two young children.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
